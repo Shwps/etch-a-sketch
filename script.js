8640f593f856a0f979b86fc7f35f5e3444b4cda1
William Ekelund <bwekelund@gmail.com>
2022-02-23 18:31:51 +0100

    Added functionality to change size of grid

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i < 16 ** 2; i++) {
-  const div = document.createElement("div");
-  div.classList.add("gridElement");
-  div.setAttribute("style", "background:; width:auto; height:auto; ");
-  div.addEventListener("mouseenter", () => div.classList.add("drawn"));
-  container.appendChild(div);
+const userInputSize = document.querySelector("#size").value;
+
+const saveButton = document.querySelector(".save");
+
+saveButton.addEventListener("click", () => updateGrid());
+
+function updateGrid (){
+  let gridSize = prompt("What size?");
+  establishGrid(gridSize);
 }
 
+
+function establishGrid (gridSize){
+  while(container.firstChild){
+    container.removeChild(container.firstChild);
+  }
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const div = document.createElement("div");
+    container.setAttribute("style", `grid-template-columns: repeat(${gridSize}, 1fr);`)
+    div.classList.add("gridElement");
+    div.setAttribute("style", "background:; width:auto; height:auto; ");
+    div.addEventListener("click", () => div.classList.add("drawn"));
+    container.appendChild(div);
+  }
+}
+
+
 const gridElements = document.querySelectorAll(".gridElement");
+
+
