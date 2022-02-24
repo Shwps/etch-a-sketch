7248c6113d34a0f51bd33a65423d2b023ecd966c
William Ekelund <bwekelund@gmail.com>
2022-02-24 19:50:42 +0100

    added logic so that you can draw while holding down m1 and moving your mouse around, instead of only on space at the time. Thanks to @michalosman for inspiration for the solution.

--- script.js
+++ script.js
@@ -4,29 +4,44 @@ const userInputSize = document.querySelector("#size").value;
 
 const saveButton = document.querySelector(".save");
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
+let gridSize = 16;
+
 saveButton.addEventListener("click", () => updateGrid());
 
-function updateGrid (){
-  let gridSize = prompt("What size?");
+function updateGrid() {
+  do {
+    gridSize = prompt("What size?");
+  } while (gridSize < 0 || gridSize > 100);
   establishGrid(gridSize);
 }
 
-
-function establishGrid (gridSize){
-  while(container.firstChild){
+function establishGrid(gridSize) {
+  while (container.firstChild) {
     container.removeChild(container.firstChild);
   }
   for (let i = 0; i < gridSize ** 2; i++) {
-    const div = document.createElement("div");
-    container.setAttribute("style", `grid-template-columns: repeat(${gridSize}, 1fr);`)
-    div.classList.add("gridElement");
-    div.setAttribute("style", "background:; width:auto; height:auto; ");
-    div.addEventListener("click", () => div.classList.add("drawn"));
-    container.appendChild(div);
+    const gridElement = document.createElement("div");
+    container.setAttribute(
+      "style",
+      `grid-template-columns: repeat(${gridSize}, 1fr);`
+    );
+    gridElement.classList.add("gridElement");
+    gridElement.setAttribute("style", "background:; width:auto; height:auto; ");
+    gridElement.addEventListener("mouseover", changeColor);
+    gridElement.addEventListener("mousedown", changeColor);
+    container.appendChild(gridElement);
   }
 }
 
 
+function changeColor (e) {
+  if(e.type === "mouseover" && !mouseDown) return;
+  e.target.style.backgroundColor = "black";
+
+}
+
 const gridElements = document.querySelectorAll(".gridElement");
-
-
